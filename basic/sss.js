7bec2747a3f45c1a56668290027b96370758e1ba
siyeon <aeiou0302@icloud.com>
2023-11-17 21:29:08 +0900

    2023-11-17 First Commit

---
+++ basic/sss.js
@@ -0,0 +1,9 @@
+
+function addNumbers(){
+    //let num1 = document.getElementById("num1");
+    //let num2 = document.getElementById("num2");
+
+    let sum = parseInt(num1.value) + parseInt(num2.value);
+    document.getElementById("result").innerHTML = `Result: ${sum}`;
+
+}
